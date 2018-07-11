347103625cfb29b07e04b7a5da295abc2c9f2049
Midhun <vmidhun@gmail.com>
2018-07-11 22:32:57 +0530

    New Comit
    Added Components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { ErrorComponent } from './error/error.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentHomeComponent } from './student-home/student-home.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { FeesComponent } from './fees/fees.component';
+import { AttendenceComponent } from './attendence/attendence.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    ErrorComponent,
+    StudentComponent,
+    TeacherComponent,
+    StudentHomeComponent,
+    AdmissionComponent,
+    FeesComponent,
+    AttendenceComponent
   ],
   imports: [
     BrowserModule
